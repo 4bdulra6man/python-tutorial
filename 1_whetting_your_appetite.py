e3d9dc33327c8cf664aef21cddbb576db8f3f3a6
abdulrahman <abdulrahman.ali1422@gmail.com>
2025-09-20 11:07:22 +0300

    feat(tutorial): summarize "Whetting Your Appetite" section

---
+++ 1_whetting_your_appetite.py
@@ -0,0 +1,5 @@
+# Python uses high-level data types, allowing to express complex operations in a single statement.
+# Statements groupting is done by indentation.
+# No need for variables, or argument types declaration.
+# Python is dynamically typed, meaning that the type of a variable is determined at runtime.
+# By the way, the language is named after the BBC show “Monty Python’s Flying Circus” and has nothing to do with reptiles. Making references to Monty Python skits in documentation is not only allowed, it is encouraged!
