0caa236eba9953943b7267339c8d2809f2a88ce5
abdulrahman <abdulrahman.ali1422@gmail.com>
2025-09-20 11:15:16 +0300

    feat(tutorial): summarize "Using the Python Interpreter" section

---
+++ 2_Using_the_Python_Interpreter.py
@@ -0,0 +1,10 @@
+# Python is usually installed at /usr/local/bin/python3.13 or /usr/local/python on Unix-like systems.
+# You can exit out of python interpreter by typing exit() or quit() or pressing Ctrl-D (on Unix) or Ctrl-Z (on Windows).
+# Control+P check if the interactive editor is working.
+# By default, Python source files are treated as encoded in UTF-8.
+# To declare an encoding other than UTF-8, a special comment should be placed at the top of the file, such as:
+# -*- coding: <encoding name> -*-
+# One exception to the first line rule is when the source code starts with a UNIX “shebang” line. In this case, the encoding declaration should be added as the second line of the file. For example:
+#!/usr/bin/env python3
+# -*- coding: cp1252 -*-
+
